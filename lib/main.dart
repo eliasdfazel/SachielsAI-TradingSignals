dd2963829d2bd48293de5f6568cebd985d5d48d0
Elias Fazel <elias.fazel@gmail.com>
2022-05-23 07:34:15 -0700

    Initializing Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,88 @@
 import 'package:flutter/material.dart';
-import 'package:sachiel/dashboard/ui/interface.dart';
+import 'package:sachiel/resources/colors_resources.dart';
 import 'package:sachiel/resources/strings_resources.dart';
 
 void main() {
-
-
-
-  runApp(const MyApp());
+  runApp(const EntryConfigurations());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class EntryConfigurations extends StatefulWidget {
+  const EntryConfigurations({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<EntryConfigurations> createState() => _EntryConfigurationsState();
+}
+
+class _EntryConfigurationsState extends State<EntryConfigurations> {
+
   @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
-      title: StringsResources.applicationName(),
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const DashboardInterface(),
-    );
+    return SafeArea(
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: StringsResources.applicationName(),
+          color: ColorsResources.primaryColor,
+          theme: ThemeData(
+            fontFamily: 'Sans',
+            colorScheme: ColorScheme.fromSwatch()
+                .copyWith(secondary: ColorsResources.primaryColor),
+            pageTransitionsTheme: const PageTransitionsTheme(builders: {
+              TargetPlatform.android: ZoomPageTransitionsBuilder(),
+              TargetPlatform.iOS: ZoomPageTransitionsBuilder(),
+            }),
+          ),
+          home: Stack(
+            children: [
+              // Gradient Background
+              Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(17),
+                      topRight: Radius.circular(17),
+                      bottomLeft: Radius.circular(17),
+                      bottomRight: Radius.circular(17)),
+                  gradient: LinearGradient(
+                      colors: [
+                        ColorsResources.light,
+                        ColorsResources.lightestBlue,
+                      ],
+                      begin: FractionalOffset(0.0, 0.0),
+                      end: FractionalOffset(1.0, 0.0),
+                      stops: [0.0, 1.0],
+                      transform: GradientRotation(45),
+                      tileMode: TileMode.clamp),
+                ),
+              ),
+              // Rounded Borders
+              Container(
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(17),
+                        topRight: Radius.circular(17),
+                        bottomLeft: Radius.circular(17),
+                        bottomRight: Radius.circular(17)),
+                    border: Border(
+                        top: BorderSide(
+                          color: Colors.black,
+                          width: 7,
+                        ),
+                        bottom: BorderSide(
+                          color: Colors.black,
+                          width: 7,
+                        ),
+                        left: BorderSide(
+                          color: Colors.black,
+                          width: 7,
+                        ),
+                        right: BorderSide(
+                          color: Colors.black,
+                          width: 7,
+                        )),
+                    color: Colors.transparent),
+              ),
+            ],
+          ),
+        ));
   }
 }
-
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:sachiel/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const EntryConfigurations());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
