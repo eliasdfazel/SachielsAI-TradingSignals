cbc403c983771b48f7164024064c59ace1ddea0a
Elias Fazel <elias.fazel@gmail.com>
2022-05-24 07:40:16 -0700

    Initializing Project

--- lib/dashboard/ui/interface.dart
+++ lib/dashboard/ui/interface.dart
@@ -30,78 +30,80 @@ class _DashboardInterfaceState extends State<DashboardInterface> {
   @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: StringsResources.applicationName(),
-        color: ColorsResources.primaryColor,
-        theme: ThemeData(
-          fontFamily: 'Ubuntu',
-          colorScheme: ColorScheme.fromSwatch().copyWith(secondary: ColorsResources.primaryColor),
-          backgroundColor: ColorsResources.dark,
-          pageTransitionsTheme: const PageTransitionsTheme(builders: {
-            TargetPlatform.android: ZoomPageTransitionsBuilder(),
-            TargetPlatform.iOS: ZoomPageTransitionsBuilder(),
-          }),
-        ),
-        home: Scaffold(
+    return SafeArea(
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: StringsResources.applicationName(),
+          color: ColorsResources.primaryColor,
+          theme: ThemeData(
+            fontFamily: 'Ubuntu',
+            colorScheme: ColorScheme.fromSwatch().copyWith(secondary: ColorsResources.primaryColor),
             backgroundColor: ColorsResources.dark,
-            body:Stack(
-              fit: StackFit.expand,
-              children: [
-                // Gradient Background
-                Container(
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(17),
-                        topRight: Radius.circular(17),
-                        bottomLeft: Radius.circular(17),
-                        bottomRight: Radius.circular(17)
-                    ),
-                    gradient: LinearGradient(
-                        colors: [
-                          ColorsResources.primaryColorDarkest,
-                          ColorsResources.primaryColorDarkest,
-                        ],
-                        begin: FractionalOffset(0.0, 0.0),
-                        end: FractionalOffset(1.0, 0.0),
-                        stops: [0.0, 1.0],
-                        transform: GradientRotation(45),
-                        tileMode: TileMode.clamp
-                    ),
-                  ),
-                ),
-                // Rounded Borders
-                Container(
-                  decoration: const BoxDecoration(
+            pageTransitionsTheme: const PageTransitionsTheme(builders: {
+              TargetPlatform.android: ZoomPageTransitionsBuilder(),
+              TargetPlatform.iOS: ZoomPageTransitionsBuilder(),
+            }),
+          ),
+          home: Scaffold(
+              backgroundColor: ColorsResources.dark,
+              body:Stack(
+                fit: StackFit.expand,
+                children: [
+                  // Gradient Background
+                  Container(
+                    decoration: const BoxDecoration(
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(17),
                           topRight: Radius.circular(17),
                           bottomLeft: Radius.circular(17),
-                          bottomRight: Radius.circular(17)),
-                      border: Border(
-                          top: BorderSide(
-                            color: ColorsResources.dark,
-                            width: 7,
-                          ),
-                          bottom: BorderSide(
-                            color: ColorsResources.dark,
-                            width: 7,
-                          ),
-                          left: BorderSide(
-                            color: ColorsResources.dark,
-                            width: 7,
-                          ),
-                          right: BorderSide(
-                            color: ColorsResources.dark,
-                            width: 7,
-                          )
+                          bottomRight: Radius.circular(17)
                       ),
-                      color: Colors.transparent
+                      gradient: LinearGradient(
+                          colors: [
+                            ColorsResources.primaryColorDarkest,
+                            ColorsResources.primaryColorDarkest,
+                          ],
+                          begin: FractionalOffset(0.0, 0.0),
+                          end: FractionalOffset(1.0, 0.0),
+                          stops: [0.0, 1.0],
+                          transform: GradientRotation(45),
+                          tileMode: TileMode.clamp
+                      ),
+                    ),
                   ),
-                ),
-              ],
-            )
-        )
+                  // Rounded Borders
+                  Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(17),
+                            topRight: Radius.circular(17),
+                            bottomLeft: Radius.circular(17),
+                            bottomRight: Radius.circular(17)),
+                        border: Border(
+                            top: BorderSide(
+                              color: ColorsResources.dark,
+                              width: 7,
+                            ),
+                            bottom: BorderSide(
+                              color: ColorsResources.dark,
+                              width: 7,
+                            ),
+                            left: BorderSide(
+                              color: ColorsResources.dark,
+                              width: 7,
+                            ),
+                            right: BorderSide(
+                              color: ColorsResources.dark,
+                              width: 7,
+                            )
+                        ),
+                        color: Colors.transparent
+                    ),
+                  ),
+                ],
+              )
+          )
+      )
     );
   }
 
