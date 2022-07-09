35740c23dbb671efdd08e44f584480b8fa380723
Elias Fazel <elias.fazel@gmail.com>
2022-07-09 15:54:43 -0700

    Firebase Support Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:sachiel/entry_configurations.dart';
+import 'package:sachiel/firebase_options.dart';
 
 void main() async {
 
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
       Phoenix(
