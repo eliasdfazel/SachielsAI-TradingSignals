8aa67867bf9e6a19f4a54a50d9d2997f47909839
Elias Fazel <elias.fazel@gmail.com>
2022-07-11 13:46:20 -0700

    [Description] Initializing Authentications
    [ChangeLog]
    # Phone Number Authentication Added (Not Tested)
    # Entry Configurations -> Welcome Views Finished
    # Phone Number Authentication User Interface Added
    # Google Authentication Added
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/entry_configurations.dart
+++ lib/entry_configurations.dart
@@ -2,7 +2,7 @@
  * Copyright © 2022 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 7/11/22, 1:28 PM
+ * Last modified 7/11/22, 1:46 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -18,7 +18,6 @@ import 'package:sachiel/resources/colors_resources.dart';
 import 'package:sachiel/resources/strings_resources.dart';
 import 'package:sachiel/utils/authentication/authentication_process.dart';
 import 'package:sachiel/utils/navigations/navigation_commands.dart';
-import 'package:sachiel/utils/ui/display.dart';
 import 'package:sachiel/utils/ui/system_bars.dart';
 
 class EntryConfigurations extends StatefulWidget {
@@ -117,34 +116,39 @@ class _EntryConfigurationsState extends State<EntryConfigurations> with Authenti
                               )
                           )
                       ),
-                      const Positioned(
-                        top: 59,
-                        left: 13,
-                        right: 13,
-                        child: Image(
-                          image: AssetImage("logo.png"),
-                          height: 339,
-                          fit: BoxFit.fitHeight,
+                      Align(
+                        alignment: Alignment.center,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.fromLTRB(0, 31, 0, 13),
+                              child: Image(
+                                image: AssetImage("logo.png"),
+                                height: 339,
+                                fit: BoxFit.fitHeight,
+                              ),
+                            ),
+                            phoneNumberAuthentication,
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 13, 0, 73),
+                              child: InkWell(
+                                onTap: () {
+
+                                  authenticationsProcess.startPhoneNumberAuthentication(phoneNumberController.text, this);
+
+                                },
+                                child: const Image(
+                                  image: AssetImage("entrance_next.png"),
+                                  height: 239,
+                                  fit: BoxFit.fitHeight,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      phoneNumberAuthentication,
-                      Positioned(
-                        bottom: 79,
-                        left: 13,
-                        right: 13,
-                        child: InkWell(
-                          onTap: () {
-
-                            authenticationsProcess.startPhoneNumberAuthentication(phoneNumberController.text, this);
-
-                          },
-                          child: const Image(
-                            image: AssetImage("entrance_next.png"),
-                            height: 239,
-                            fit: BoxFit.fitHeight,
-                          ),
-                        )
-                      ),
+                      )
                     ]
                 )
             )
@@ -168,152 +172,148 @@ class _EntryConfigurationsState extends State<EntryConfigurations> with Authenti
     if (firebaseAuthentication.currentUser!.phoneNumber == null) {
       debugPrint("Phone Number Not Authenticated");
 
-      phoneNumberAuthentication = Positioned(
-          top: (displayHeight(context) / 2) - 39,
-          left: 37,
-          child: SizedBox(
-              height: 113,
-              width: 373,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                      height: 37,
-                      width: 137,
-                      child: Stack(
-                          children: [
-                            Blur(
-                              blur: 5,
-                              blurColor: ColorsResources.dark,
-                              colorOpacity: 0.1,
-                              borderRadius: BorderRadius.circular(9),
-                              child: const SizedBox(
-                                height: 37,
-                                width: 137,
+      phoneNumberAuthentication = SizedBox(
+          height: 113,
+          width: 373,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                  height: 37,
+                  width: 137,
+                  child: Stack(
+                      children: [
+                        Blur(
+                          blur: 5,
+                          blurColor: ColorsResources.dark,
+                          colorOpacity: 0.1,
+                          borderRadius: BorderRadius.circular(9),
+                          child: const SizedBox(
+                            height: 37,
+                            width: 137,
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(9),
+                                  topRight: Radius.circular(9),
+                                  bottomLeft: Radius.circular(9),
+                                  bottomRight: Radius.circular(9)
                               ),
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                  borderRadius: const BorderRadius.only(
-                                      topLeft: Radius.circular(9),
-                                      topRight: Radius.circular(9),
-                                      bottomLeft: Radius.circular(9),
-                                      bottomRight: Radius.circular(9)
-                                  ),
-                                  border: Border.all(
-                                    color: ColorsResources.black,
-                                    width: 2,
-                                  ),
-                                  color: ColorsResources.dark.withOpacity(0.37)
+                              border: Border.all(
+                                color: ColorsResources.black,
+                                width: 2,
                               ),
-                              child: Align(
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    StringsResources.phoneNumber(),
-                                    style: const TextStyle(
-                                        color: ColorsResources.premiumLight,
-                                        fontSize: 19
-                                    ),
-                                  )
-                              ),
-                            )
-                          ]
-                      )
-                  ),
-                  SizedBox(
-                      height: 73,
-                      width: 373,
-                      child: Stack(
-                          children: [
-                            Blur(
-                              blur: 7,
-                              blurColor: ColorsResources.dark,
-                              colorOpacity: 0.1,
-                              borderRadius: BorderRadius.circular(17),
-                              child: const SizedBox(
-                                height: 72,
-                                width: 373,
-                              ),
-                            ),
-                            TextField(
-                              controller: phoneNumberController,
-                              textAlign: TextAlign.left,
-                              textDirection: TextDirection.ltr,
-                              textAlignVertical: TextAlignVertical.center,
-                              maxLines: 1,
-                              cursorColor: ColorsResources.primaryColor,
-                              autofocus: false,
-                              keyboardType: TextInputType.phone,
-                              textInputAction: TextInputAction.done,
-                              style: TextStyle(
-                                  color: ColorsResources.light,
-                                  fontSize: 37.0,
-                                  shadows: [
-                                    Shadow(
-                                        color: ColorsResources.primaryColorLighter.withOpacity(0.71),
-                                        blurRadius: 17,
-                                        offset: const Offset(0.0, 3.0)
-                                    )
-                                  ]
-                              ),
-                              decoration: InputDecoration(
-                                alignLabelWithHint: true,
-                                border: const OutlineInputBorder(
-                                    borderSide: BorderSide(color: Colors.black, width: 3.0),
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(17),
-                                        topRight: Radius.circular(17),
-                                        bottomLeft: Radius.circular(17),
-                                        bottomRight: Radius.circular(17)
-                                    ),
-                                    gapPadding: 5
+                              color: ColorsResources.dark.withOpacity(0.37)
+                          ),
+                          child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                StringsResources.phoneNumber(),
+                                style: const TextStyle(
+                                    color: ColorsResources.premiumLight,
+                                    fontSize: 19
                                 ),
-                                enabledBorder: const OutlineInputBorder(
-                                    borderSide: BorderSide(color: Colors.black, width: 3.0),
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(17),
-                                        topRight: Radius.circular(17),
-                                        bottomLeft: Radius.circular(17),
-                                        bottomRight: Radius.circular(17)
-                                    ),
-                                    gapPadding: 5
-                                ),
-                                focusedBorder: const OutlineInputBorder(
-                                    borderSide: BorderSide(color: Colors.black, width: 3.0),
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(17),
-                                        topRight: Radius.circular(17),
-                                        bottomLeft: Radius.circular(17),
-                                        bottomRight: Radius.circular(17)
-                                    ),
-                                    gapPadding: 5
-                                ),
-                                errorBorder: const OutlineInputBorder(
-                                    borderSide: BorderSide(color: Colors.red, width: 3.0),
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(17),
-                                        topRight: Radius.circular(17),
-                                        bottomLeft: Radius.circular(17),
-                                        bottomRight: Radius.circular(17)
-                                    ),
-                                    gapPadding: 5
-                                ),
-                                errorText: warningNoticePhoneNumber,
-                                contentPadding: const EdgeInsets.fromLTRB(19, 21, 19, 21),
-                                hintText: StringsResources.phoneNumberHint(),
-                              ),
-                              onSubmitted: (phoneNumber) {
-
-
-
-                              },
-                            )
-                          ]
-                      )
+                              )
+                          ),
+                        )
+                      ]
                   )
-                ],
+              ),
+              SizedBox(
+                  height: 73,
+                  width: 373,
+                  child: Stack(
+                      children: [
+                        Blur(
+                          blur: 7,
+                          blurColor: ColorsResources.dark,
+                          colorOpacity: 0.1,
+                          borderRadius: BorderRadius.circular(17),
+                          child: const SizedBox(
+                            height: 72,
+                            width: 373,
+                          ),
+                        ),
+                        TextField(
+                          controller: phoneNumberController,
+                          textAlign: TextAlign.left,
+                          textDirection: TextDirection.ltr,
+                          textAlignVertical: TextAlignVertical.center,
+                          maxLines: 1,
+                          cursorColor: ColorsResources.primaryColor,
+                          autofocus: false,
+                          keyboardType: TextInputType.phone,
+                          textInputAction: TextInputAction.done,
+                          style: TextStyle(
+                              color: ColorsResources.light,
+                              fontSize: 37.0,
+                              shadows: [
+                                Shadow(
+                                    color: ColorsResources.primaryColorLighter.withOpacity(0.71),
+                                    blurRadius: 17,
+                                    offset: const Offset(0.0, 3.0)
+                                )
+                              ]
+                          ),
+                          decoration: InputDecoration(
+                            alignLabelWithHint: true,
+                            border: const OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black, width: 3.0),
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(17),
+                                    topRight: Radius.circular(17),
+                                    bottomLeft: Radius.circular(17),
+                                    bottomRight: Radius.circular(17)
+                                ),
+                                gapPadding: 5
+                            ),
+                            enabledBorder: const OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black, width: 3.0),
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(17),
+                                    topRight: Radius.circular(17),
+                                    bottomLeft: Radius.circular(17),
+                                    bottomRight: Radius.circular(17)
+                                ),
+                                gapPadding: 5
+                            ),
+                            focusedBorder: const OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black, width: 3.0),
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(17),
+                                    topRight: Radius.circular(17),
+                                    bottomLeft: Radius.circular(17),
+                                    bottomRight: Radius.circular(17)
+                                ),
+                                gapPadding: 5
+                            ),
+                            errorBorder: const OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red, width: 3.0),
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(17),
+                                    topRight: Radius.circular(17),
+                                    bottomLeft: Radius.circular(17),
+                                    bottomRight: Radius.circular(17)
+                                ),
+                                gapPadding: 5
+                            ),
+                            errorText: warningNoticePhoneNumber,
+                            contentPadding: const EdgeInsets.fromLTRB(19, 21, 19, 21),
+                            hintText: StringsResources.phoneNumberHint(),
+                          ),
+                          onSubmitted: (phoneNumber) {
+
+
+
+                          },
+                        )
+                      ]
+                  )
               )
+            ],
           )
       );
 
@@ -325,7 +325,7 @@ class _EntryConfigurationsState extends State<EntryConfigurations> with Authenti
 
     } else {
 
-      navigateTo(context, const DashboardInterface());
+      // navigateTo(context, const DashboardInterface());
 
     }
 
