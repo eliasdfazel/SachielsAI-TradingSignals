f238c1029e5557c83db19730640c801f34fdb5bb
Elias Fazel <elias.fazel@gmail.com>
2022-07-12 18:04:38 -0700

    [Description] Designing Dashboard -> Last Signal Details
    [ChangeLog]
    # First Card
    # Signals Data Structure Added
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/data/signals_data_structure.dart
+++ lib/data/signals_data_structure.dart
@@ -2,7 +2,7 @@
  * Copyright © 2022 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 7/12/22, 5:25 PM
+ * Last modified 7/12/22, 6:01 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -11,6 +11,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SignalsDataStructure {
+
+  static const String tradeCommandName = "tradeCommand";
+  static const String tradeMarketPairName = "tradeMarketPair";
+
+  static const String tradeAccuracyPercentageName = "tradeAccuracyPercentage";
+
+  static const String tradeEntryPriceName = "tradeEntryPrice";
+  static const String tradeTakeProfitName = "tradeTakeProfit";
+  static const String tradeStopLossName = "tradeStopLoss";
+
+  static const String tradeLotSizeName = "tradeLotSize";
+  static const String tradeProfitAmountName = "tradeProfitAmount";
+
+  static const String tradeTimeframeName = "tradeTimeframe";
+
+  static const String tradeTimestampName = "tradeTimestamp";
 
   Map<String, dynamic> signalsDocumentData = <String, dynamic>{};
 
@@ -23,7 +39,58 @@ class SignalsDataStructure {
   /// Buy Or Sell Command
   String tradeCommand() {
 
-    return signalsDocumentData[""];
-}
+    return signalsDocumentData[SignalsDataStructure.tradeCommandName];
+  }
+  /// Trade Market Pair/Name (For Example, XAUUSD or Crude Oil or GOOGL)
+  String tradeMarketPair() {
+
+    return signalsDocumentData[SignalsDataStructure.tradeMarketPairName];
+  }
+
+  /// Trade Accuracy In Percentage
+  String tradeAccuracyPercentage() {
+
+    return signalsDocumentData[SignalsDataStructure.tradeAccuracyPercentageName];
+  }
+
+  /// Trade Entry Price
+  String tradeEntryPrice() {
+
+    return signalsDocumentData[SignalsDataStructure.tradeEntryPriceName];
+  }
+  /// Trade Take Profit Price
+  String tradeTakeProfit() {
+
+    return signalsDocumentData[SignalsDataStructure.tradeTakeProfitName];
+  }
+  /// Trade Stop Loss Price
+  String tradeStopLoss() {
+
+    return signalsDocumentData[SignalsDataStructure.tradeStopLossName];
+  }
+
+  /// Trade Lot Size Unit (Recommended)
+  String tradeLotSize() {
+
+    return signalsDocumentData[SignalsDataStructure.tradeLotSizeName];
+  }
+
+  /// Trade Profit Amount In Dollar
+  String tradeProfitAmount() {
+
+    return signalsDocumentData[SignalsDataStructure.tradeProfitAmountName];
+  }
+
+  /// Trade Timeframe (Interval - 1 Hour, 4 Hours, Daily & etc)
+  String tradeTimeframe() {
+
+    return signalsDocumentData[SignalsDataStructure.tradeTimeframeName];
+  }
+
+  /// Trade Timestamp
+  String tradeTimestamp() {
+
+    return signalsDocumentData[SignalsDataStructure.tradeTimestampName];
+  }
 
 }
