9a0b15a811b1d71afcca1c8d69b04e179a2bc31f
Elias Fazel <elias.fazel@gmail.com>
2022-07-30 00:53:09 -0700

    [Description] Designing Dashboard -> Last Signal Details
    [ChangeLog]
    # Third Card - Share View... (No Functionality)
    # Second Card - Details Of Last Signal Completed
    # First Card - Overview Of Last Signal Completed
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/dashboard/ui/sections/last_signal_details.dart
+++ lib/dashboard/ui/sections/last_signal_details.dart
@@ -2,7 +2,7 @@
  * Copyright © 2022 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 7/30/22, 12:51 AM
+ * Last modified 7/30/22, 12:52 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -861,7 +861,9 @@ class _LastSignalDetailsState extends State<LastSignalDetails> {
                   InkWell(
                     onTap: () {
 
-
+                      /*
+                       * Share Signal Data as A Snapshot (Design Share Snapshot Widget)
+                       */
 
                     },
                     child: Container(
