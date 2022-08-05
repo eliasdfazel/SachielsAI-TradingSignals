e04457cadd43049a60ded95874ebc30d2b055ddc
Elias Fazel <elias.fazel@gmail.com>
2022-08-05 05:22:59 -0700

    [Description] Designing Dashboard -> Last Signal Details
    [ChangeLog]
    # Initializing Introduction Slides with Liquid Swipe
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/dashboard/ui/sections/latest_signals_overview.dart
+++ lib/dashboard/ui/sections/latest_signals_overview.dart
@@ -2,15 +2,16 @@
  * Copyright © 2022 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 8/3/22, 3:06 AM
+ * Last modified 8/5/22, 2:24 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
  */
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:sachiel/data/signals_data_structure.dart';
 import 'package:sachiel/resources/colors_resources.dart';
-import 'package:sachiel/utils/ui/system_bars.dart';
 
 class LatestSignalsOverview extends StatefulWidget {
 
@@ -22,11 +23,15 @@ class LatestSignalsOverview extends StatefulWidget {
 }
 class _LatestSignalsOverviewState extends State<LatestSignalsOverview> {
 
+  ScrollController scrollController = ScrollController();
+
+  Widget latestSignalsDetails = Container();
+
   @override
   void initState() {
     super.initState();
 
-    changeColor(ColorsResources.black, ColorsResources.black);
+    retrieveLatestSignalDetails();
 
   }
 
@@ -38,17 +43,129 @@ class _LatestSignalsOverviewState extends State<LatestSignalsOverview> {
   @override
   Widget build(BuildContext context) {
 
-    return      SizedBox(
-      width: 311,
-      height: 179,
-      child: ListView(
-        padding: const EdgeInsets.fromLTRB(19, 0, 19, 73),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 7, 0, 7),
+      child: SizedBox(
+          height: 173,
+          width: double.infinity,
+          child: latestSignalsDetails
+      )
+    );
+  }
+
+  void retrieveLatestSignalDetails() {
+    debugPrint("Retrieve Latest Signals Details");
+
+    FirebaseFirestore.instance
+        .collection("SachielsSignals")
+        // .limit(7)
+        .orderBy("tradeTimestamp")
+        .get().then((QuerySnapshot querySnapshot) {
+
+          List<SignalsDataStructure> signalsDataStructure = [];
+
+          for (QueryDocumentSnapshot queryDocumentSnapshot in querySnapshot.docs) {
+
+            signalsDataStructure.add(SignalsDataStructure(queryDocumentSnapshot));
+
+          }
+
+          prepareLatestSignalsDetails(signalsDataStructure);
+
+        },
+        onError: (e) => {
+
+        });
+
+  }
+
+  void prepareLatestSignalsDetails(List<SignalsDataStructure> signalsDataStructure) {
+
+    List<Widget> aLatestSignal = <Widget>[];
+
+    for (var signalDataStructureItem in signalsDataStructure) {
+
+      aLatestSignal.add(signalDataStructureItemView(signalDataStructureItem));
+
+    }
+
+    setState(() {
+
+      latestSignalsDetails = ListView(
+        padding: const EdgeInsets.fromLTRB(19, 0, 19, 0),
         physics: const BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
-        children: [
+        controller: scrollController,
+        children: aLatestSignal,
+      );
 
-        ],
-      ),
+    });
+
+  }
+
+  Widget signalDataStructureItemView(SignalsDataStructure signalsDataStructure) {
+    debugPrint("Latest Signals Details Data: ${signalsDataStructure.tradeCommand()} ${signalsDataStructure.tradeMarketPair()}");
+
+    return SizedBox(
+        height: 173,
+        width: 119,
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(17),
+                  topRight: Radius.circular(17),
+                  bottomLeft: Radius.circular(17),
+                  bottomRight: Radius.circular(17)
+              ),
+              border: const Border(
+                  top: BorderSide(
+                    color: ColorsResources.primaryColorDarker,
+                    width: 1.3,
+                  ),
+                  bottom: BorderSide(
+                    color: ColorsResources.primaryColorDarker,
+                    width: 1.3,
+                  ),
+                  left: BorderSide(
+                    color: ColorsResources.primaryColorDarker,
+                    width: 1.3,
+                  ),
+                  right: BorderSide(
+                    color: ColorsResources.primaryColorDarker,
+                    width: 1.3,
+                  )
+              ),
+              color: ColorsResources.premiumDark,
+              boxShadow: [
+                BoxShadow(
+                  color: ColorsResources.primaryColorLightest.withOpacity(0.13),
+                  blurRadius: 19,
+                  offset: const Offset(0, 0)
+                )
+              ]
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            fit: StackFit.loose,
+            children: [
+
+              /* Start - Percentage */
+              Align(
+                alignment: AlignmentDirectional.center,
+                child: Text(
+                  "%",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 173,
+                      fontFamily: "Handwriting"
+                  ),
+                )
+              )
+              /* End - Percentage */
+
+            ],
+          ),
+        )
     );
   }
 
