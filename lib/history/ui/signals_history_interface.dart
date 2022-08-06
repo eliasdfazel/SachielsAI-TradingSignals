7cd2b59735a8c751597a8e06ec1939cba0ca63af
Elias Fazel <elias.fazel@gmail.com>
2022-08-06 02:28:46 -0700

    [Description] Designing Dashboard -> Last Signal Details
    [ChangeLog]
    # Initializing Introduction Slides with Liquid Swipe
    # Signals History Added
    # The Latest Signals Added
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/history/ui/signals_history_interface.dart
+++ lib/history/ui/signals_history_interface.dart
@@ -2,7 +2,7 @@
  * Copyright © 2022 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 8/6/22, 2:27 AM
+ * Last modified 8/6/22, 2:28 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -295,7 +295,7 @@ class _SignalsHistoryInterfaceState extends State<SignalsHistoryInterface> {
 
     FirebaseFirestore.instance
         .collection("SachielsSignals")
-        .limit(7)
+        .limit(37)
         .orderBy("tradeTimestamp")
         .get().then((QuerySnapshot querySnapshot) {
 
