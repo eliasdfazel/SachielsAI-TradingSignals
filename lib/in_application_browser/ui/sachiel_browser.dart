a71d6246c6f7d886976b38e55c3d243a132bcae5
Elias Fazel <elias.fazel@gmail.com>
2022-08-19 05:44:27 -0700

    [Description] Designing Dashboard -> Last Signal Details
    [ChangeLog]
    # Initializing Sachiel Browser
    # Initializing Introduction Slides with Liquid Swipe
    # Initializing Academy Sections
    
    [Build][ChangeLog]
    # Libraries Updated

---
+++ lib/in_application_browser/ui/sachiel_browser.dart
@@ -0,0 +1,10 @@
+/*
+ * Copyright © 2022 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 8/19/22, 5:40 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
