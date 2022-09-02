b86278221d3e0aa112a72b2397cfda187427b707
Elias Fazel <elias.fazel@gmail.com>
2022-09-02 09:28:33 -0700

    [Description] Designing Dashboard -> Last Signal Details
    [ChangeLog]
    # Sachiels Store Added for Plan Subscriptions
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,26 @@
  * Copyright © 2022 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 7/31/22, 7:39 PM
+ * Last modified 9/2/22, 9:24 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
  */
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:sachiel/entry_configurations.dart';
 import 'package:sachiel/firebase_options.dart';
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+
+  await Firebase.initializeApp();
+
+}
+
 
 void main() async {
 
@@ -21,9 +29,11 @@ void main() async {
 
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
-  await Firebase.initializeApp(
+  var firebaseInitialized = await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
 
   runApp(
       Phoenix(
