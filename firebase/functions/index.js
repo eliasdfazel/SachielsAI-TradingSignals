9f5a1e0e30ba44d8c60c1a83033c2187bcab0462
Elias Fazel <elias.fazel@gmail.com>
2022-09-15 08:30:19 -0700

    [Description] Designing Dashboard -> Last Signal Details
    [ChangeLog]
    # Firebase Cloud Function & Emulator Added
    
    [Build][ChangeLog]
    # Libraries Updated

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -21,4 +21,34 @@ exports.platinumListener = functions.firestore
 
       const tradeCommand = documentData.tradeCommand;
 
+//var message = {
+//
+//        android: {
+//            ttl: (3600 * 1000) * (1), // 1 Hour in Milliseconds
+//
+//            priority: 'high',
+//        },
+//
+//        data: {
+//            "selfDisplayName": selfDisplayName,
+//            "selfUid": selfUid,
+//            "publicCommunityAction": publicCommunityAction,
+//            "nameOfCountry": nameOfCountry,
+//            "vicinityLatitude": vicinityLatitude,
+//            "vicinityLongitude": vicinityLongitude,
+//            "publicCommunityName": publicCommunityName,
+//            "notificationLargeIcon": notificationLargeIcon,
+//            "messageContent": messageContent,
+//            "imageMessage": '' + imageMessage
+//        },
+//
+//        topic: notificationTopic
+//    };
+//
+//    return admin.messaging().send(message).then((response) => {
+//        console.log('Successfully Sent ::: ', response);
+//    }).catch((error) => {
+//        console.log('Error Sending Message ::: ', error);
+//    });
+
 });
