a9a861f105e3a8d5a3edb8deee5d2a25998a8c1c
Elias Fazel <elias.fazel@gmail.com>
2022-11-16 10:00:48 -0800

    [Description] Designing Dashboard -> Last Signal Details
    [ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/signals/signal_details/ui/signal_details_interface.dart
+++ lib/signals/signal_details/ui/signal_details_interface.dart
@@ -2,7 +2,7 @@
  * Copyright © 2022 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 11/16/22, 9:58 AM
+ * Last modified 11/16/22, 9:59 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -1486,7 +1486,8 @@ class _SignalsDetailsInterfaceState extends State<SignalsDetailsInterface> {
       child: InkWell(
         onTap: () {
 
-          launchUrl(Uri.parse(StringsResources.marketChartLink(widget.signalsDataStructure.tradeMarketPair())));
+          launchUrl(Uri.parse(StringsResources.marketChartLink(widget.signalsDataStructure.tradeMarketPair())),
+              mode: LaunchMode.externalApplication);
 
         },
         child: const Image(
