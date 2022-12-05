9b0e5ac0a03186acecaec09694dbd6973f57f93d
Elias Fazel <elias.fazel@gmail.com>
2022-12-05 10:53:17 -0800

    [Description]
    [ChangeLog]
    ~
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/dashboard/ui/dashboard_interface.dart
+++ lib/dashboard/ui/dashboard_interface.dart
@@ -8,7 +8,7 @@
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
  */
-ptor.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
