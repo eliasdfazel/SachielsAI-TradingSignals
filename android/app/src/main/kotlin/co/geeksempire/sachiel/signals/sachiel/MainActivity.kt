80483caf7ad947666a7b941517779a49b2c9369d
Elias Fazel <elias.fazel@gmail.com>
2022-12-07 05:25:23 -0800

    [Description]
    [ChangeLog]
    ~ Filter By Market Type
    ~ Optimized Splash Screen Navigation
    
    [Build][ChangeLog]
    # Libraries Updated

--- android/app/src/main/kotlin/co/geeksempire/sachiel/signals/sachiel/MainActivity.kt
+++ android/app/src/main/kotlin/co/geeksempire/sachiel/signals/sachiel/MainActivity.kt
@@ -1,13 +1,16 @@
 package co.geeksempire.sachiel.signals.sachiel
 
 import android.view.WindowManager.LayoutParams
+import co.geeksempire.sachiel.signals.sachiel.BuildConfig
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        window.addFlags(LayoutParams.FLAG_SECURE)
+        if (!BuildConfig.DEBUG) {
+            window.addFlags(LayoutParams.FLAG_SECURE)
+        }
         super.configureFlutterEngine(flutterEngine)
     }
 
