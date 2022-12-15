2f9dc28cc3a7fe0d72787407d5306e8530ac933f
Elias Fazel <elias.fazel@gmail.com>
2022-12-15 07:20:43 -0800

    [Description]
    [ChangeLog]
    ~ Academy Dynamic Height Added
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@
  * https://opensource.org/licenses/MIT
  */
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -15,9 +16,17 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:sachiel/entry_configurations.dart';
 import 'package:sachiel/firebase_options.dart';
+import 'package:sachiel/in_application_store/ui/sachiel_digital_store.dart';
+import 'package:sachiel/remote/remote_configurations.dart';
+import 'package:sachiel/resources/strings_resources.dart';
+import 'package:sachiel/utils/data/texts.dart';
+import 'package:sachiel/utils/io/file_io.dart';
 
 @pragma('vm:entry-point')
-Future<void> firebaseMessagingBackgroundHandler(RemoteMessage remoteMessage) async {}
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage remoteMessage) async {
+  debugPrint("Sachiels Signal Received: ${remoteMessage.data}");
+
+}
 
 void main() async {
 
@@ -36,6 +45,27 @@ void main() async {
     sound: true,
   );
 
+  fileExist(StringsResources.filePurchasingPlan).then((fileExist) => {
+
+    if (fileExist) {
+      readFileOfTexts(StringsResources.fileNamePurchasingPlan, "TXT").then((purchasedPlan) => {
+
+        Future.delayed(Duration.zero, () {
+          debugPrint("Purchased Plan: ${capitalizeFirstCharacter(purchasedPlan.split(".").first)}");
+
+          FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+          firebaseMessaging.subscribeToTopic(capitalizeFirstCharacter(purchasedPlan.split(".").first));
+
+        })
+
+      })
+
+    }
+
+  });
+
+  prototypeProcess();
+
   runApp(
       Phoenix(
           child: const MaterialApp(
@@ -43,5 +73,30 @@ void main() async {
           )
       )
   );
+
+}
+
+void prototypeProcess() {
+
+  RemoteConfigurations remoteConfigurations = RemoteConfigurations();
+
+  remoteConfigurations.initialize().then((firebaseRemoteConfigurations) {
+
+    firebaseRemoteConfigurations.activate().then((value) async {
+
+      String sachielPrincipal = firebaseRemoteConfigurations.getString(RemoteConfigurations.sachielPrincipal);
+
+      if (sachielPrincipal.contains(FirebaseAuth.instance.currentUser!.email.toString())) {
+
+        FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+        firebaseMessaging.subscribeToTopic(SachielsDigitalStore.platinumTopic);
+        firebaseMessaging.subscribeToTopic(SachielsDigitalStore.goldTopic);
+        firebaseMessaging.subscribeToTopic(SachielsDigitalStore.palladiumTopic);
+
+      }
+
+    });
+
+  });
 
 }
---
+++ lib/utils/data/texts.dart
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2022 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 12/15/22, 6:13 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+String capitalizeFirstCharacter(String inputText) {
+
+  return "${inputText[0].toUpperCase()}${inputText.substring(1)}";
+}
